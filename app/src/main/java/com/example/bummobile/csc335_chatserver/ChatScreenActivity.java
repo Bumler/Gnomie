b862014590ca2e5eccfd92f97184c13be277c43c
Henry Bulmer <hbulmer@callutheran.edu>
2016-10-18 01:19:24 -0700

    Beginning to create chat screen
    Logic added to login to navigate to chat screen. Working on succesfully
    implementing a floating action button

---
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/ChatScreenActivity.java
@@ -0,0 +1,21 @@
+package com.example.bummobile.csc335_chatserver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by Bum Mobile on 10/18/2016.
+ */
+
+public class ChatScreenActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
--- app/src/main/java/com/example/bummobile/csc335_chatserver/LoginActivity.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.bummobile.csc335_chatserver;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 /**
@@ -13,18 +15,33 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity{
     Button register;
+    Button logon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        InitComponents();
+    }
+
+    private void InitComponents(){
         register = (Button)findViewById(R.id.register_button);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        logon = (Button)findViewById(R.id.login);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, ChatScreenActivity.class);
                 startActivity(i);
             }
         });
