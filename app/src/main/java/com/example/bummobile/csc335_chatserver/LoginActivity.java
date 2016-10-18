05f9bbfac162dd154e0a5a71ec08081f22e696b2
Henry Bulmer <hbulmer@callutheran.edu>
2016-10-17 22:11:32 -0700

    Added All Files
    ...

---
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.bummobile.csc335_chatserver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+/**
+ * Created by Bum Mobile on 10/17/2016.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    Button register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        register = (Button)findViewById(R.id.register_button);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.example.bummobile.csc335_chatserver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+/**
+ * Created by Bum Mobile on 10/17/2016.
+ */
+
+public class RegisterActivity extends AppCompatActivity{
+    Button registerUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        registerUser = (Button)findViewById(R.id.registerUser);
+        registerUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
