81a1d13a6d20a40109de03cd3a24589da9075340
Henry Bulmer <hbulmer@callutheran.edu>
2016-10-20 15:41:43 -0700

    Added Email Logic to chatscreen activity
    Needs testing with an actual phone with an email application. Using this
    tutorial
    https://www.tutorialspoint.com/android/android_sending_email.htm

--- app/src/main/java/com/example/bummobile/csc335_chatserver/ChatScreenActivity.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/ChatScreenActivity.java
@@ -1,21 +1,61 @@
 package com.example.bummobile.csc335_chatserver;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Bum Mobile on 10/18/2016.
  */
 
 public class ChatScreenActivity extends AppCompatActivity{
+
+    FloatingActionButton email;
+
     protected void onCreate(Bundle savedInstanceState) {
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatscreen);
 
+        initComponents();
+    }
+
+    private void initComponents(){
+        email = (FloatingActionButton) findViewById(R.id.sendEmail);
+        email.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View vew){
+                sendEmail();
+            }
+        });
+    }
+
+    protected void sendEmail(){
+        Log.i("Send email", "");
+        String[] TO = {""};
+        String[] CC = {""};
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("message/rfc822");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email.", "");
+        }
+        catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(ChatScreenActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
