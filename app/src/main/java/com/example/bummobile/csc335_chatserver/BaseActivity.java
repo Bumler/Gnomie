7bd8a9ad565c3100bff48a28a150659bc8739d16
Henry Bulmer <hbulmer@callutheran.edu>
2016-10-25 15:58:49 -0700

    Attempting to implement Firebase registration
    Added in basic activity to the java classes. Registration is currently
    crashing on load based upon trying to instanciate a null object.

---
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/BaseActivity.java
@@ -0,0 +1,40 @@
+package com.example.bummobile.csc335_chatserver;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bum Mobile on 10/25/2016.
+ */
+
+import android.app.ProgressDialog;
+import android.support.annotation.VisibleForTesting;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressDialog();
+    }
+
+}
--- app/src/main/java/com/example/bummobile/csc335_chatserver/RegisterActivity.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/RegisterActivity.java
@@ -2,17 +2,45 @@ package com.example.bummobile.csc335_chatserver;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Bum Mobile on 10/17/2016.
  */
 
-public class RegisterActivity extends AppCompatActivity{
+public class RegisterActivity extends BaseActivity{
     Button registerUser;
+
+    TextInputEditText username;
+    TextInputEditText password;
+    TextInputEditText passwordConf;
+    TextInputEditText email;
+
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    // [START declare_auth_listener]
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    // [END declare_auth_listener]
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +48,103 @@ public class RegisterActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        // [START auth_state_listener]
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // [START_EXCLUDE]
+                //updateUI(user);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END auth_state_listener]
+
+        initComponents();
+
+    }
+
+    // [START on_start_add_listener]
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void initComponents(){
+        username = (TextInputEditText) findViewById(R.id.new_username);
+        password = (TextInputEditText)findViewById(R.id.password);
+        passwordConf = (TextInputEditText)findViewById(R.id.password_confirm);
+        email = (TextInputEditText)findViewById(R.id.new_email);
+
         registerUser = (Button)findViewById(R.id.registerUser);
         registerUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                createAccount(email.getText().toString(), password.getText().toString());
                 Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
                 startActivity(i);
             }
         });
+    }
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String emailTest = email.getText().toString();
+        if (TextUtils.isEmpty(emailTest)) {
+            email.setError("Required.");
+            valid = false;
+        } else {
+            email.setError(null);
+        }
+
+        String passwordTest = password.getText().toString();
+        if (TextUtils.isEmpty(passwordTest)) {
+            password.setError("Required.");
+            valid = false;
+        } else {
+            password.setError(null);
+        }
+
+        return valid;
     }
 }
