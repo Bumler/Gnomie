ba0b4675d3ecdfe5dc9ffe79af60e0e5d5624dcc
Henry Bulmer <hbulmer@callutheran.edu>
2016-10-25 20:45:53 -0700

    Succesfully implemented login
    Currently is succesful at logging in users given valid credentials. It
    also skips the log in screen if the user has already succesfully logged
    in. Need to test the skip part though to make sure it is logging in with
    the correct user.

--- app/src/main/java/com/example/bummobile/csc335_chatserver/LoginActivity.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/LoginActivity.java
@@ -3,31 +3,58 @@ package com.example.bummobile.csc335_chatserver;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * Created by Bum Mobile on 10/17/2016.
  */
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends BaseActivity{
     Button register;
     Button logon;
+
+    TextInputEditText inputEmail;
+    TextInputEditText inputPassword;
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //if the user is already logged in it will move to the chat screen before displaying the login screen
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, ChatScreenActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
 
         InitComponents();
     }
 
     private void InitComponents(){
+        inputEmail = (TextInputEditText)findViewById(R.id.email);
+        inputPassword = (TextInputEditText)findViewById(R.id.password);
+
         register = (Button)findViewById(R.id.register_button);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,8 +68,43 @@ public class LoginActivity extends AppCompatActivity{
         logon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(LoginActivity.this, ChatScreenActivity.class);
-                startActivity(i);
+//                Intent i = new Intent(LoginActivity.this, ChatScreenActivity.class);
+//                startActivity(i);
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), R.string.error_email, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), R.string.error_pw, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                showProgressDialog();
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                hideProgressDialog();
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, ChatScreenActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
             }
         });
     }
