3eaa0a73c257a57149e2b2a9b93cb0a92423c81b
Henry Bulmer <hbulmer@callutheran.edu>
2016-11-07 18:44:39 -0800

    Avaliable Chats now displays 'rooms'
    These rooms are taken from the database and can be generated by the
    user.

--- app/src/main/java/com/example/bummobile/csc335_chatserver/AvaliableChatsPOP.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/AvaliableChatsPOP.java
@@ -3,12 +3,38 @@ package com.example.bummobile.csc335_chatserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Bum Mobile on 11/7/2016.
  */
 
 public class AvaliableChatsPOP extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> arrayAdapter;
+    private ArrayList<String> list_of_rooms = new ArrayList<>();
+    private Button add_room;
+    private EditText room_name;
+
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
 
     protected void onCreate(Bundle savedInstanceState) {
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -32,6 +58,59 @@ public class AvaliableChatsPOP extends AppCompatActivity {
     }
 
     private void initComponents(){
+        add_room = (Button) findViewById(R.id.btn_add_room);
+        room_name = (EditText) findViewById(R.id.room_name_edittext);
+        listView = (ListView) findViewById(R.id.avaliable_chats);
+
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_of_rooms);
+
+        listView.setAdapter(arrayAdapter);
+
+        add_room.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put(room_name.getText().toString(),"");
+                root.updateChildren(map);
+
+            }
+        });
+
+        root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Set<String> set = new HashSet<String>();
+                Iterator i = dataSnapshot.getChildren().iterator();
+
+                while (i.hasNext()){
+                    set.add(((DataSnapshot)i.next()).getKey());
+                }
+
+                list_of_rooms.clear();
+                list_of_rooms.addAll(set);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                Intent intent = new Intent(getApplicationContext(),Chat_Room.class);
+//                intent.putExtra("room_name",((TextView)view).getText().toString() );
+//                intent.putExtra("user_name",name);
+//                startActivity(intent);
+//            }
+//        });
 
     }
+
 }
--- app/src/main/java/com/example/bummobile/csc335_chatserver/ChatScreenActivity.java
+++ app/src/main/java/com/example/bummobile/csc335_chatserver/ChatScreenActivity.java
@@ -11,6 +11,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Bum Mobile on 10/18/2016.
  */
